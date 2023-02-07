6953dd52364e1a0c426ec9a6ca0632b2c3979746
Egor Mozheiko <gopherbopher123@gmail.com>
2023-02-07 16:15:23 +0300

    tst 26

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Testing Stuff Here</h1>
       <p>eks dee 123</p>
+      <p>oh boy, the title will change now, check this out</p>
     </div>
   );
 }
