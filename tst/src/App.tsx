3b193861787578fae45171bf0694d627bd0b6a64
Egor Mozheiko <gopherbopher123@gmail.com>
2023-02-07 17:12:50 +0300

    tst 52

--- tst/src/App.tsx
+++ tst/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import pic from "./assets/react.svg";
 
 function App() {
   return (
     <div className="App">
       <h1>Testing Stuff Here</h1>
+      <img src={pic} />
     </div>
   );
 }
