b27b8f3e5ab11da4a3ae27ba8c2032bebfd78c40
Egor Mozheiko <gopherbopher123@gmail.com>
2023-02-08 19:46:58 +0300

    tst 57

--- tst/src/App.tsx
+++ tst/src/App.tsx
@@ -4,7 +4,8 @@ import pic from "./assets/react.svg";
 function App() {
   return (
     <div className="App">
-      <h1>Testing Some Stuff Here, because why not</h1>
+      <h1>Testing Some Stuff Here</h1>
+      <p>eks dee</p>
       <img src={pic} />
     </div>
   );
