a9bf400d600fc5c1f6840603d0dbb345b3f006db
Egor Mozheiko <gopherbopher123@gmail.com>
2023-02-16 01:29:49 +0300

    tst 67

--- tst/src/App.tsx
+++ tst/src/App.tsx
@@ -1,13 +1,67 @@
 import "./App.css";
 import pic from "./assets/react.svg";
+import { BrowserRouter, Route, Routes, Outlet, Link } from "react-router-dom";
+
+const env = process.env.NODE_ENV;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Don't mind me</h1>
-      <h2>just testing some stuff here.</h2>
-      <img src={pic} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={
+            <div>
+              <p>LOOK, HEADER HERE xD</p>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route
+            index
+            path=""
+            element={
+              <div className="App">
+                <h1>Don't mind me</h1>
+                <h2>just testing some stuff.</h2>
+                <p>main page</p>
+                <img src={pic} />
+                <Link to="/first">1</Link>
+                <Link to="/second">2</Link>
+                <Link to="/third">3</Link>
+              </div>
+            }
+          />
+          <Route
+            path="first"
+            element={
+              <div className="App">
+                <h2>just testing some stuff.</h2>
+                <p>first page</p>
+              </div>
+            }
+          />
+          <Route
+            path="second"
+            element={
+              <div className="App">
+                <h2>just testing some stuff.</h2>
+                <p>second page</p>
+                {env}
+              </div>
+            }
+          />
+          <Route
+            path="third"
+            element={
+              <div className="App">
+                <h2>just testing some stuff.</h2>
+                <p>third</p>
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- tst/vite.config.ts
+++ tst/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+
+const env = process.env.NODE_ENV;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: env === "production" ? "/dpltst/" : "./",
   plugins: [react()],
-  base: "./",
 });
